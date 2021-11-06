5bb0ad36314c9395cfc583da68639a3ea192bffe
whyusb@gmail.com <whyusb@gmail.com>
2021-11-06 21:42:15 +0800

    feat: open in browser

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"path"
 	"path/filepath"
+	"runtime"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -25,23 +28,21 @@ type spaHandler struct {
 // file located at the index path on the SPA handler will be served. This
 // is suitable behavior for serving an SPA (single page application).
 func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// get the absolute path to prevent directory traversal
-	path, err := filepath.Abs(r.URL.Path)
+
+	localpath, err := os.Getwd()
 	if err != nil {
-		// if we failed to get the absolute path respond with a 400 bad request
-		// and stop
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// prepend the path with the path to the static directory
-	path = filepath.Join(h.staticPath, path)
+	path := filepath.Join(localpath, h.staticPath)
 
-	// check whether a file exists at the given path
-	_, err = os.Stat(path)
+	reqpath := filepath.Join(path, r.URL.Path)
+	_, err = os.Stat(reqpath)
 	if os.IsNotExist(err) {
 		// file does not exist, serve index.html
-		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		http.ServeFile(w, r, filepath.Join(path, h.indexPath))
 		return
 	} else if err != nil {
 		// if we got an error (that wasn't that the file doesn't exist) stating the
@@ -54,16 +55,43 @@ func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
+/*获取当前文件执行的路径*/
+func GetCurPath() string {
+	ex, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := path.Dir(ex)
+	return dir
+}
+
+func openBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 func main() {
 	router := mux.NewRouter()
-
-	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		// an example API handler
-		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-	})
-
+	fmt.Println(GetCurPath())
 	spa := spaHandler{staticPath: "dist", indexPath: "index.html"}
 	router.PathPrefix("/").Handler(spa)
+
+	fmt.Println("服务启动成功，请用浏览器打开 http://127.0.0.1:5555 ")
+	openBrowser("http://127.0.0.1:5555")
 
 	srv := &http.Server{
 		Handler: router,
